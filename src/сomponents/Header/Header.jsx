299c6cef31460e491e81e6aa44e0dd647dfc55b0
JL-Huawei_Graznov <graznov@me.com>
2024-02-08 23:55:14 +0300

    29

--- src/сomponents/Header/Header.jsx
+++ src/сomponents/Header/Header.jsx
@@ -1,9 +1,9 @@
 import './Header.css'
 function Header(){
     return(
-        <>
+        <div className='header'>
             Personal Journal
-        </>
+        </div>
     )
 }
 export default Header
