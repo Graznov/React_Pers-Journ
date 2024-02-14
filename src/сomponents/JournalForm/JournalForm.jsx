1c2b3e032e19410dd1a6a62f9860c397e5334eb5
JL-Huawei_Graznov <graznov@me.com>
2024-02-14 16:48:40 +0300

    41- Изменение стилей (Стилизация)

--- src/сomponents/JournalForm/JournalForm.jsx
+++ src/сomponents/JournalForm/JournalForm.jsx
@@ -1,13 +1,41 @@
 import './JournalForm.css'
 import Button from "../Button/Button.jsx";
-import App from "../../App.jsx";
+import {useState} from "react";
 function JournalForm({ onSubmit }){ // через onSubmit отправляются данные в App
+
+    const [ formValidState, setFormValidState ] = useState({
+        title : true,
+        post : true,
+        date : true
+    })
 
 
     const addJournalItem = (e) => {
         e.preventDefault()
         const formData = new FormData(e.target)
         const formProps = Object.fromEntries(formData)
+        let isFormValid = true
+        if(!formProps.title?.trim().length){
+            setFormValidState(state => ({ ...state, title: false }))
+            isFormValid = false
+        } else {
+            setFormValidState(state => ({ ...state, title: true }))
+        }
+        if(!formProps.post?.trim().length){
+            setFormValidState(state => ({ ...state, post: false }))
+            isFormValid = false
+        } else {
+            setFormValidState(state => ({ ...state, post: true }))
+        }
+        if(!formProps.date){
+            setFormValidState(state => ({ ...state, date: false }))
+            isFormValid = false
+        } else {
+            setFormValidState(state => ({ ...state, date: true }))
+        }
+        if(!isFormValid) {
+            return
+        }
 
         console.log(formProps)
         onSubmit(formProps)
@@ -16,10 +44,10 @@ function JournalForm({ onSubmit }){ // через onSubmit отправляют�
 
     return(
             <form className='journal-form' onSubmit={addJournalItem}>
-                <input type="text" name='title' />
-                <input type="date" name='date'/>
+                <input type="text" name='title' style={{border: formValidState.title ? undefined : '2px solid red'}}/>
+                <input type="date" name='date' style={{border: formValidState.date ? undefined : '2px solid red'}}/>
                 <input type="text" name='tag'/>
-                <textarea name='text' id='' cols='30' rows='10'></textarea>
+                <textarea name='post' id='' cols='30' rows='10' style={{border: formValidState.post ? undefined : '1px solid red'}}></textarea>
                 <Button text='Сохранить' onClick={()=>{
                     console.log('CLICK')
                 }}/>
